45774b50b87eaf4a6753fbdfe0f8a0a61e5f2268
Ayman Essobhi <a.essobhi@ideracloud.com>
2024-11-18 23:53:44 +0100

    create the book entity

--- book-scape/src/main/java/com/essobhi/bookscape/domain/Book.java
+++ book-scape/src/main/java/com/essobhi/bookscape/domain/Book.java
@@ -1,4 +1,39 @@
 package com.essobhi.bookscape.domain;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="BS-BOOK")
 public class Book {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+
 }
