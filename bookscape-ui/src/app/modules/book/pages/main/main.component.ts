396675f0b264aa014306c0bda7104caf4be90378
Ayman Essobhi <a.essobhi@ideracloud.com>
2024-11-28 23:51:55 +0100

    implement the book list page

--- bookscape-ui/src/app/modules/book/pages/main/main.component.ts
+++ bookscape-ui/src/app/modules/book/pages/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MenuComponent} from "../../components/menu/menu.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuComponent,
+    RouterOutlet
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
