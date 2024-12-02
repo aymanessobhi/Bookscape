150bab485db8b629f9231a3153eb63e8131059f1
Ayman Essobhi <a.essobhi@ideracloud.com>
2024-12-02 23:12:28 +0100

    updated

--- book-scape/src/main/java/com/essobhi/bookscape/dto/BaseDto.java
+++ book-scape/src/main/java/com/essobhi/bookscape/dto/BaseDto.java
@@ -1,10 +1,15 @@
 package com.essobhi.bookscape.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -15,8 +20,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BaseDto<D>{
     private Integer id;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
     private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
     private Integer lastModifiedBy;
 }
+
